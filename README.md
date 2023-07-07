fb4b8654628d22262628d1b031b3cd1224c8f108
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-07-07 23:10:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Laser Chess engine
+Written in TypeScript
+
+## Running
+
+1. Develop
+  ```
+  npm run dev
+  ```
+
+2. Build
+  ```
+  npm run build
+  ```
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import chalk from 'chalk';
+
+const NAME: string = "TS Laser Chess";
+
+
+function main() {
+  console.log(chalk.bold.cyan(`[${NAME}]`) + " Started");
+  console.log(chalk.bold.cyan(`[${NAME}]`) + " Hello :)");
+  console.log(chalk.bold.cyan(`[${NAME}]`) + " Finished");
+}
+
+main();
