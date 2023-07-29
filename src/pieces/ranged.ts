7e9e7d5b2c0dc67b836444244a187a679abaf36e
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-07-29 23:10:34 +0200

    Create `NotImplementedError`

--- src/pieces/ranged.ts
+++ src/pieces/ranged.ts
@@ -1,24 +1,23 @@
+import { NotImplementedError } from "utils/error.js";
 import { BoardVector2d } from "geometry";
 import { Piece } from "./piece.js";
 
 class RangedMovement /** extends PieceMovement */ {
-    constructor(ranged: Piece) {
+  constructor(ranged: Piece) {
 
-    }
+  }
 
-    public iterate_squares(origin: BoardVector2d) {
+  public iterateSquares(origin: BoardVector2d) {
+    throw new NotImplementedError('iterateSquares() method is not implemented yet');
+  }
 
-    }
+  public getAllMoves(): BoardVector2d[][] {
+    throw new NotImplementedError('getAllMoves() method is not implemented yet');
+  }
 
-    public getAllMoves(): BoardVector2d[][] {
-        return [];
-    }
-
-    public getLegalMoves(): BoardVector2d[][] {
-        return [];
-    }
-
-
+  public getLegalMoves(): BoardVector2d[][] {
+    throw new NotImplementedError('getLegalMoves() method is not implemented yet');
+  }
 }
 
 class RangedPiece extends Piece {
