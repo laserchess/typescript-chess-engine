0ad104cd597257a6afda65c9e94b9d1ef83a7904
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-08-16 13:58:53 +0200

    Rename some modules to `PascalCase`

--- src/core/PieceMoveType.ts
+++ src/core/PieceMoveType.ts
@@ -2,7 +2,7 @@ export const enum PieceMoveType {
   Move              = 1 << 0,
   Capture           = 1 << 1,
   KingSideCastling  = 1 << 2,
-  QueebSideCastling = 1 << 3,
+  QueenSideCastling = 1 << 3,
   Promotion         = 1 << 4,
   Check             = 1 << 5,
   Checkmate         = 1 << 6,
