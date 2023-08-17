9f883a43cc49f2652ce15fb85a9f022f16178b9b
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-08-17 20:29:08 +0200

    Remove getter and setter from `DirectedPiece`

--- src/pieces/DirectedPiece.ts
+++ src/pieces/DirectedPiece.ts
@@ -2,17 +2,9 @@ import { BoardVector2d } from "geometry";
 import { Direction, Piece, PieceOpitons } from "pieces";
 
 export class DirectedPiece extends Piece {
-  protected _direction?: Direction;
+  public direction?: Direction;
 
   public constructor(position: BoardVector2d, playerId: number, board: Board, options: PieceOpitons) {
     super(position, playerId, board, options);
-  }
-
-  public set direction(direction: Direction) {
-    this._direction = direction;
-  }
-
-  public get direction(): Direction {
-    return this._direction!;
   }
 }
