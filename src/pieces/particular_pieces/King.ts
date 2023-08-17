0f19c269e21db1df534ce2f02c1ad0ce84ffeab3
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-08-17 19:45:57 +0200

    Remove piece suffix from class names

---
+++ src/pieces/particular_pieces/King.ts
@@ -0,0 +1,96 @@
+import { PieceMoveType } from "core/move_type.js";
+import { BoardVector2d } from "geometry";
+import { Piece, PieceMovement, PieceOpitons, PieceType } from "pieces";
+import { CloseRangeMovement } from "pieces/around_piece.js";
+import { Knight } from "pieces/particular_pieces/knight.js";
+
+
+export class King extends Piece {
+  private _kingRook?: Piece;
+  private _queenRook?: Piece;
+
+  public constructor(position: BoardVector2d, playerId: number, board: Board) {
+    const options: PieceOpitons =
+    {
+      pieceType: PieceType.KING,
+      movement: new KingMovement(board)
+    }
+    super(position, playerId, board, options);
+    this.movement.piece = this;
+  }
+
+  public set kingRook(rook: Piece) {
+    this._kingRook = this.kingRook;
+  }
+
+  public set queenRook(rook: Piece) {
+    this._queenRook = this.kingRook;
+  }
+
+  public get kingRook(): Piece {
+    return this._kingRook!;
+  }
+
+  public get queenRook(): Piece {
+    return this._queenRook!;
+  }
+  
+}
+
+export class KingMovement extends CloseRangeMovement {
+
+  public isCastlingLegal(castling: PieceMoveType): boolean {
+    let potentialRook: Piece;
+    let piece: King = this.piece as King;
+    if (castling === PieceMoveType.KingSideCastling) {
+      potentialRook = piece.kingRook;
+    }
+    else if (castling === PieceMoveType.QueebSideCastling) {
+      potentialRook = piece.queenRook;
+    }
+    else {
+      return false;
+    }
+    
+    if (
+      potentialRook.pieceType === PieceType.ROOK
+      && !potentialRook.wasMoved()
+      && !this.piece.wasMoved()
+    ) {
+      const fromKingUnitVector: BoardVector2d = this.piece.position.sub(potentialRook.position).createUnitVector();
+      let currentPosition: BoardVector2d = this.piece.position;
+
+      for (let i = 0; i < 2; i++) {
+        if (this.board.isCheckAt(currentPosition, this.piece.playerId)) {
+          return false;
+        }
+        currentPosition = currentPosition.add(fromKingUnitVector);
+      }
+
+      currentPosition = this.piece.position;
+      while (currentPosition !== potentialRook.position) {
+        if (!this.board.canMoveTo(currentPosition, this.piece)) {
+          return false;
+        }
+        currentPosition = currentPosition.add(fromKingUnitVector);
+      }
+      return true
+    }
+    return false;
+  }
+
+  public updateMoves(): void {
+    super.updateMoves();
+    if (this.isCastlingLegal(PieceMoveType.KingSideCastling)) {
+      this._legalMoves.push(this.piece.position.add(new BoardVector2d(2, 0)))
+      this._allMoves.push(this.piece.position.add(new BoardVector2d(2, 0)))
+    }
+    else if (this.isCastlingLegal(PieceMoveType.QueebSideCastling)) {
+      this._legalMoves.push(this.piece.position.add(new BoardVector2d(-2, 0)))
+      this._allMoves.push(this.piece.position.add(new BoardVector2d(-2, 0)))
+    }
+  }
+}
+
+
+
---
+++ src/pieces/particular_pieces/Mirror.ts
@@ -0,0 +1,39 @@
+import { BoardVector2d } from "geometry";
+import { DirectedPiece, Direction, PieceOpitons, PieceType } from "pieces";
+import { CloseRangeMovement } from "pieces/around_piece.js";
+
+export class Mirror extends DirectedPiece {
+  public constructor(position: BoardVector2d, playerId: number, board: Board) {
+    let options: PieceOpitons =
+    {
+      pieceType: PieceType.MIRROR,
+      movement: new MirrorMovement(board)
+    }
+    super(position, playerId, board, options);
+    this.movement.piece = this
+  }
+  public set direction(direction: Direction) {
+    if (direction % 2 === 0) {
+      throw new Error("Direction for MirrorPiece have to be diagonal.");
+    }
+    super.direction = direction;
+  }
+}
+
+export class MirrorMovement extends CloseRangeMovement {
+
+  public turnClockwise(): void {
+    const piece: Mirror = this.piece as Mirror;
+    piece.direction = Direction.turnDoubleRight(piece.direction)
+  }
+
+  public turnAnticlockwise(): void {
+    const piece: Mirror = this.piece as Mirror;
+    piece.direction = Direction.turnDoubleLeft(piece.direction)
+  }
+
+  public updateMoves(): void {
+    super.updateMoves();
+    this._capturableMoves.length = 0;  
+  }
+}
