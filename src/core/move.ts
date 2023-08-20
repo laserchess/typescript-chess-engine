3282d9f4f40854a92389553dc662760f97fb4b25
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-08-20 21:13:49 +0200

    Split `MoveType` into 2 related enums

--- src/core/move.ts
+++ src/core/move.ts
@@ -1,20 +1,25 @@
 import { BoardVector2d, Rotation } from "@lc/geometry";
 import { Piece } from "@lc/pieces";
 
-export const enum MoveType {
+export const enum PieceMoveType {
   Move              = 1 << 0,
   Capture           = 1 << 1,
-  KingSideCastling  = 1 << 2,
-  QueenSideCastling = 1 << 3,
-  Promotion         = 1 << 4,
-  Check             = 1 << 5,
-  Checkmate         = 1 << 6,
-  Stalemate         = 1 << 7,
-  Draw              = 1 << 8,
-  Rotation          = 1 << 9,
-  Lasermate         = 1 << 10,
-  LaserFired        = 1 << 11,
-  LaserCooldown     = 1 << 12
+  RangedCapture     = 1 << 2,
+  EnPassant         = 1 << 3,
+  KingSideCastling  = 1 << 4,
+  QueenSideCastling = 1 << 5,
+  Promotion         = 1 << 6,
+  Rotation          = 1 << 7
+}
+
+export const enum BoardMoveType {
+  Check             = 1 << 8,
+  Checkmate         = 1 << 9,
+  Stalemate         = 1 << 10,
+  Draw              = 1 << 11,
+  Lasermate         = 1 << 12,
+  LaserFired        = 1 << 13,
+  LaserCooldown     = 1 << 14
 }
 
 export interface Move {
@@ -22,6 +27,6 @@ export interface Move {
   origin: BoardVector2d,
   destination: BoardVector2d,
   captured: Piece,
-  moveType: MoveType,
+  moveType: PieceMoveType & BoardMoveType,
   rotation: Rotation
 }
