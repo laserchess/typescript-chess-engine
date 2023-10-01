ee9d7981b4efa894f1958b6960e2f5ec078a6a94
kpiotr6 <piotrek@komlan.pl>
2023-10-01 21:00:49 +0200

    Added tests for `Queen`.

--- tests/pieces/Queen.test.ts
+++ tests/pieces/Queen.test.ts
@@ -2,19 +2,19 @@ import { Board, Generator, Move, MoveType, Player } from "@lc/core"
 import { BoardVector2d } from "geometry/Vector2d.js"
 import { PieceType } from "pieces/Piece.js";
 
-describe("Bishop", () => {
+describe("Queen", () => {
   const board = new Board(8, 8);
 
   describe("movement", () => {
     describe("updateMoves", () => {
       test("No other pieces", () => {
-        const bishop = Generator.createStandardPiece(
+        const queen = Generator.createStandardPiece(
           {
             position: new BoardVector2d(1, 1),
             playerId: Player.White,
             board: board
           },
-          PieceType.BISHOP
+          PieceType.QUEEN
         )
         const movesLegal: Set<Partial<Move>> = new Set([
           { destination: new BoardVector2d(0, 0), moveType: MoveType.Move },
@@ -26,22 +26,36 @@ describe("Bishop", () => {
           { destination: new BoardVector2d(7, 7), moveType: MoveType.Move },
           { destination: new BoardVector2d(0, 2), moveType: MoveType.Move },
           { destination: new BoardVector2d(2, 0), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 0), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 2), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 3), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 4), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 5), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 6), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 7), moveType: MoveType.Move },
+          { destination: new BoardVector2d(0, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(2, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(3, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(4, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(5, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(6, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(7, 1), moveType: MoveType.Move },
         ])
 
-        bishop.movement.updateMoves();
+        queen.movement.updateMoves();
         for (const singleMove of movesLegal) {
-          expect(bishop.movement.legalMoves).toContainEqual(singleMove);
+          expect(queen.movement.legalMoves).toContainEqual(singleMove);
         }
-        expect(bishop.movement.illegalMoves.length).toBe(0);
+        expect(queen.movement.illegalMoves.length).toBe(0);
       })
       test("Enemy/friendly piece", () => {
-        const bishop = Generator.createStandardPiece(
+        const queen = Generator.createStandardPiece(
           {
             position: new BoardVector2d(1, 1),
             playerId: Player.White,
             board: board
           },
-          PieceType.BISHOP
+          PieceType.QUEEN
         )
         const enemyBishop = Generator.createStandardPiece(
           {
@@ -49,7 +63,7 @@ describe("Bishop", () => {
             playerId: Player.Black,
             board: board
           },
-          PieceType.BISHOP
+          PieceType.QUEEN
         )
         // You're my friend now
         const friendlyBishop = Generator.createStandardPiece(
@@ -58,7 +72,7 @@ describe("Bishop", () => {
             playerId: Player.White,
             board: board
           },
-          PieceType.BISHOP
+          PieceType.QUEEN
         )
         const movesLegal: Set<Partial<Move>> = new Set([
           { destination: new BoardVector2d(0, 0), moveType: MoveType.Move },
@@ -66,6 +80,20 @@ describe("Bishop", () => {
           { destination: new BoardVector2d(3, 3), moveType: MoveType.Move },
           { destination: new BoardVector2d(4, 4), moveType: MoveType.Move },
           { destination: new BoardVector2d(5, 5), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 0), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 2), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 3), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 4), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 5), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 6), moveType: MoveType.Move },
+          { destination: new BoardVector2d(1, 7), moveType: MoveType.Move },
+          { destination: new BoardVector2d(0, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(2, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(3, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(4, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(5, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(6, 1), moveType: MoveType.Move },
+          { destination: new BoardVector2d(7, 1), moveType: MoveType.Move },
           { destination: new BoardVector2d(6, 6), moveType: MoveType.Move | MoveType.Capture },
           // { destination: new BoardVector2d(7, 7), moveType: MoveType.Move },
           { destination: new BoardVector2d(0, 2), moveType: MoveType.Move },
@@ -79,13 +107,13 @@ describe("Bishop", () => {
 
         board.addPiece(enemyBishop);
         board.addPiece(friendlyBishop);
-        bishop.movement.updateMoves();
+        queen.movement.updateMoves();
 
         for (const singleMove of movesLegal) {
-          expect(bishop.movement.legalMoves).toContainEqual(singleMove);
+          expect(queen.movement.legalMoves).toContainEqual(singleMove);
         }
         for (const singleMove of movesIllegal) {
-          expect(bishop.movement.illegalMoves).toContainEqual(singleMove);
+          expect(queen.movement.illegalMoves).toContainEqual(singleMove);
         }
 
       })
